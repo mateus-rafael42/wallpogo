7ec2993c4a3343724b3678fd6a7dd7d3cd19f045
Mateus Rafael <10mateus02@gmail.com>
2024-12-27 22:48:34 -0300

    FIX: add hidden logic of slash screen

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const emailButton = document.querySelector('.wallpogo__button--email');
 emailButton.addEventListener('click', () => {
   window.location.href = 'mailto:mateus.rafael.ufpr@gmail.com';
 });
+
+// Assuming you have a reference to the splash screen element
+const splashScreen = document.querySelector('.splash-screen');
+
+// Set a timeout to add the hidden class after 4.2 seconds
+setTimeout(() => {
+  splashScreen.classList.add('hidden'); // Add the hidden class after the total animation time
+}, 4200); // 4.2 seconds in milliseconds
